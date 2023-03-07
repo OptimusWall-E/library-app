2f3dd82314eb0668b3c1004738f1ae9dadc981e8
OptimusWall-E <iemailedmichael@gmail.com>
2023-03-07 07:11:05 +0000

    First commit. Project set up. Initialised with React and Typescript.

---
+++ 03-frontend/react-library/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Library App using OIDC</h1>
+    </div>
+  );
+}
+
+export default App;
