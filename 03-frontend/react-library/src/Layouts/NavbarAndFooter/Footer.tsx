4f006fef4eb337a951e4d1c5ba41fbc64fca787d
OptimusWall-E <iemailedmichael@gmail.com>
2023-03-09 12:07:54 +0000

    Corrected Footer spacing for list items.

--- 03-frontend/react-library/src/Layouts/NavbarAndFooter/Footer.tsx
+++ 03-frontend/react-library/src/Layouts/NavbarAndFooter/Footer.tsx
@@ -1,9 +1,9 @@
 export const Footer = () => {
-  return(
+  return (
     <div className='main-color'>
-      <footer className='containter d-flex flex-wrap justify-content-between align-items-center py-5 main-color'>
+      <footer className='container d-flex flex-wrap justify-content-between align-items-center py-5 main-color'>
         <p className='col-md-4 mb-0 text-white'>© Library App, Inc</p>
-        <ul>
+        <ul className='nav navbar-dark col-md-4 justify-content-end'>
           <li className='nav-item'>
             <a className='nav-link px-2 text-white' href='#'>
               Home
