922144fe17e9e9de5bc07441d529b61b71b1562f
OptimusWall-E <iemailedmichael@gmail.com>
2023-03-15 12:22:19 +0000

    Added book entity. Added annotations to column names to connect them to database. Imported dependencies.

---
+++ 02-backend/spring-boot-library/src/main/java/com/libraryapp/springbootlibrary/entity/Book.java
@@ -0,0 +1,34 @@
+package com.libraryapp.springbootlibrary.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import javax.persistence.*; //for passing in everything we need for creating tables as entities
+
+@Entity
+@Table(name = "book") // "book" is the name of the table in mySQL database
+@Data // from Lombok. Automatically creates all getters and setters for book
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "copies")
+    private int copies;
+
+    @Column(name = "copies_available")
+    private int copiesAvailable;
+
+    @Column(name = "img")
+    private String img;
+}
